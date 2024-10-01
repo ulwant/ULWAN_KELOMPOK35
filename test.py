1a5463586f79926a9cf317dc22efb805c6ed830c
ulwant <ulwanterry@gmail.com>
2024-10-01 23:35:01 +0700

    Update 2 test.py

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
+# kode pembuatan nama akun pengguna
+nama_user = input("\nbuat nama akun anda: ")
+
+while nama_user == "":
+    nama_user = input("buat dulu nama akun anda: ")
+
 # mengimpor modul random agar posisi tupai dapat menjadi random
 import random
 
@@ -17,3 +23,17 @@ while True:
     print("")
     print("=" * (len(nama_user) + 38))
     print(f"Halo {nama_user}! Coba perhatikan goa dibawah ini\n{goa_kosong}\n")
+
+# function untuk membuat respon pengguna terhadap permainan
+def respon_user():
+    respon = int(input("\nApakah anda suka dengan program ini? [1-10] : "))
+
+    if respon < 7 :
+        return print("\nTerimakasih telah menilai, Kami akan selalu meningkatkan kinerja game ini")
+    else:
+        return print("\nTerimakasih telah menilai, Kami akan selalu menjaga kinerja game ini")
+    
+respon_user()
+
+# menginformasikan kepada user bahwa program telah selesai
+print("\nPROGRAM SELESAI\nTerimakasih telah memainkan game kami\n")
