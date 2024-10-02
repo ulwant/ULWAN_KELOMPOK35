515fb1c7b007678438de45b09778a7d374f014b3
isnanice <imasolekhahisna@gmail.com>
2024-10-02 18:07:17 +0700

    Update test.py

--- test.py
+++ test.py
@@ -24,6 +24,19 @@ while True:
     print("=" * (len(nama_user) + 38))
     print(f"Halo {nama_user}! Coba perhatikan goa dibawah ini\n{goa_kosong}\n")
 
+# kode yang menyatakan apakah pengguna menang atau kalah
+    pilihan_user = int(input("Menurut kamu di goa nomor berapa si tupai berada? [1 / 2 / 3 / 4]: "))
+   
+    if pilihan_user == tupai_position:
+            print(f"\n{goa}\n\nSelamat Kamu Menang 🏆")
+    else:
+            print(f"\n{goa}\n\nUncchhh kamu kalah 🙊")
+
+    # kode untuk mengulagi permainan
+    play_again = input("\napakah ingin melanjutkan gamenya lagi? [y/n]")
+    if play_again == "n":
+        break
+
 # function untuk membuat respon pengguna terhadap permainan
 def respon_user():
     respon = int(input("\nApakah anda suka dengan program ini? [1-10] : "))
